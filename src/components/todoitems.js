3eed56ef5abe70538ea4e55dbb1c3d6e58a03e09
devynqualls <58799637+devynqualls@users.noreply.github.com>
2021-07-12 16:14:37 -0400

    updates

---
+++ src/components/todoitems.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+function ToDoItems() {
+    return (
+            <div className="todo-item" >
+                <input type='checkbox' />
+                <p>Placeholder text</p>
+
+            </div>
+    )
+}
+
+export default ToDoItems
