0e25680f420b6e77e95694a8b63ea5813051f559
devynqualls <58799637+devynqualls@users.noreply.github.com>
2021-07-16 16:07:06 -0400

    fixes

---
+++ src/components/todoData.js
@@ -0,0 +1,31 @@
+const todoData = [
+
+    {
+        id: 1,
+        text: 'take out trash',
+        completed: false
+    },
+    {
+        id: 2,
+        text: 'Clean Garage',
+        completed: false
+    },
+    {
+        id: 3,
+        text: 'Do Laundry',
+        completed: false
+    },
+    {
+        id: 4,
+        text: 'Plan week',
+        completed: false
+    },
+    {
+        id: 5,
+        text: 'workout ',
+        completed: false
+    }
+
+]
+
+export default todoData
